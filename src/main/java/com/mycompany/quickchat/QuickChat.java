ee1d5cf56e3578eb93059db9fe7a8d858529e834
sne-hope <st10532795@rcconnect.edu.za>
2026-04-13 02:52:05 +0200

    Update QuickChat.java

--- src/main/java/com/mycompany/quickchat/QuickChat.java
+++ src/main/java/com/mycompany/quickchat/QuickChat.java
@@ -6,14 +6,15 @@ package com.mycompany.quickchat;
 
 import java.util.Scanner;
 
-
+//Main class for the QuickChat APP
 public class QuickChat {
 
     public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     System.out.print("Enter first name: ");
     String firstName = scan.next();
-    
+
+    //Prompt the user for their details to register   
     System.out.print("Enter last name: ");
     String lastName = scan.next();
     
@@ -31,12 +32,13 @@ public class QuickChat {
     
     System.out.println(outputmessages);
     
-    
+    //if registration is successful attempt login for the user
     if(outputmessages.contains("not correctly")){
     System.out.println("Please fix your errors and try again.");
     return; 
     }
-   
+
+    //Prompt the user for their login credentials
     System.out.print("Enter username to Login: ");
     String loginUsername = scan.next();
     
